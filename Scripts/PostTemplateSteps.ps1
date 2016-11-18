491ae0324b9f6d6017fbc3e126265a666c386df5
marrobi <marrobi@LAPTOP88>
2016-11-18 15:10:12 +0000

    Change linux config

--- Scripts/PostTemplateSteps.ps1
+++ Scripts/PostTemplateSteps.ps1
@@ -9,6 +9,6 @@ Param
 )
 
 # Assign config to Linux VM
-$node = Get-AzureRmAutomationDscNode -AutomationAccountName "Automation$ResourceGroupName" -Name "linuxvm1" -ResourceGroupName $ResourceGroupName
-Set-AzureRmAutomationDscNode -AutomationAccountName "Automation$ResourceGroupName" -Id $node.Id -NodeConfigurationName "LinuxVMConfiguration.localhost" -ResourceGroupName $ResourceGroupName -Force
+#$node = Get-AzureRmAutomationDscNode -AutomationAccountName "Automation$ResourceGroupName" -Name "linuxvm1" -ResourceGroupName $ResourceGroupName
+#Set-AzureRmAutomationDscNode -AutomationAccountName "Automation$ResourceGroupName" -Id $node.Id -NodeConfigurationName "LinuxVMConfiguration.localhost" -ResourceGroupName $ResourceGroupName -Force
 
